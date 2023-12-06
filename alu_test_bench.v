90eeec60ebcccc399a831d7203af056b733735f4
itsramazain <104790048+itsramazain@users.noreply.github.com>
2023-12-06 21:17:47 +0300

    Update alu_test_bench.v

--- alu_test_bench.v
+++ alu_test_bench.v
@@ -1,11 +1,13 @@
 `timescale 1ns / 1ps
 module test();
 	reg CLOCK_50;
-	reg [2:0]ALU_OP;
+	reg [3:0]ALU_OP;
+	reg [4:0]shmt;
 	reg [31:0]rs,rt;
 	wire zero; 
 	wire [31:0]alu_result;
-	alu arth(ALU_OP,rs,rt,alu_result,zero);
+	wire overflow,lessthan,carry;
+	alu arth(ALU_OP,rs,rt,alu_result,zero,shmt,overflow,lessthan,carry);
 	parameter CLOCK_PERIOD=2;
 	
 	
@@ -24,6 +26,7 @@ module test();
 	begin
 	rs<=32'b111;	
 	rt<=32'b101;
+	shmt<=5'd3;
 	end
 	
 	
@@ -35,16 +38,31 @@ module test();
 	begin
 	
 
-	
+	/* 0000 and 
+	0001 or 
+	0010 add
+	0011 sub
+	0100 nor
+	0101 xor
+	0110 slt
+	0111 sll
+	1000 srl
+	1001 subu
+	1011 addu
+	*/
 
 		
-	#2;ALU_OP=3'b000;
-	#2;ALU_OP=3'b001;
-	#2;ALU_OP=3'b010;
-	#2;ALU_OP=3'b011;
-	#2;ALU_OP=3'b100;
-	#2;ALU_OP=3'b101;
-	#2;ALU_OP=3'b110;
+	#2;ALU_OP=4'b000;
+	#2;ALU_OP=4'b001;
+	#2;ALU_OP=4'b010;
+	#2;ALU_OP=4'b011;
+	#2;ALU_OP=4'b100;
+	#2;ALU_OP=4'b101;
+	#2;ALU_OP=4'b110;
+	#2;ALU_OP=4'b111;
+	#2;ALU_OP=4'b1000;
+	#2;ALU_OP=4'b1001;
+	#2;ALU_OP=4'b1011;
 	
 	
 	
